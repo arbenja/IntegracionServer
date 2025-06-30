7cb0dbf00661f35c53d0af9f16311974e0bd118c
arbenja <133251497+arbenja@users.noreply.github.com>
2025-06-30 05:00:31 -0400

    Update settings.py

--- DjangoBkn/mi_producto/mi_producto/settings.py
+++ DjangoBkn/mi_producto/mi_producto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6nv8=5(-lx8tgxb@d^-pkkb5oq7l(z&vaxsj_if6bqkb1h2er=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
